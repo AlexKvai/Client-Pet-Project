e623f1208c3ff577c0eded4bfd56d43eaa0003a8
alex parfenov <alexp-2004@mail.ru>
2024-03-24 16:17:21 +0700

    Добавление metadata

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,14 @@
-import AuthPage from '@/pages/auth/AuthPage'
+import { Metadata } from 'next'
 import { FC } from 'react'
+
+import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+
+import AuthPage from '@/pages/auth/AuthPage'
+
+export const metadata: Metadata = {
+	title: 'Войти',
+	...NO_INDEX_PAGE
+}
 
 const Page: FC = () => {
 	return <AuthPage />
