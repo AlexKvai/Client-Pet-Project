84cc09a79b4e6daa8a1c7a223b66698eb234a263
alex parfenov <alexp-2004@mail.ru>
2024-03-24 18:27:00 +0700

    Обновление провайдера

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { PropsWithChildren, useState } from 'react'
 
+import AuthContextProvider from '@/store/context'
+
 export function Providers({ children }: PropsWithChildren) {
 	const [client] = useState(
 		new QueryClient({
@@ -16,9 +18,11 @@ export function Providers({ children }: PropsWithChildren) {
 	)
 
 	return (
-		<QueryClientProvider client={client}>
-			{children}
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		<AuthContextProvider>
+			<QueryClientProvider client={client}>
+				{children}
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
+		</AuthContextProvider>
 	)
 }
