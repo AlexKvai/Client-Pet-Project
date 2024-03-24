29e55cb03c91324ff8a251416acdd49a80c94198
alex parfenov <alexp-2004@mail.ru>
2024-03-24 21:43:13 +0700

    Изменена позиция Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 				<Providers>
 					<Header />
 					{children}
-					<Toaster />
+					<Toaster position='top-right' />
 					<Footer />
 				</Providers>
 			</body>
