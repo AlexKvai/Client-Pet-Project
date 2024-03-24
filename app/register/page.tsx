2f05076a6e5484fcfc864defd8c9c9a33b93babc
alex parfenov <alexp-2004@mail.ru>
2024-03-24 16:17:51 +0700

    Добавление metadata

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,14 @@
-import RegisterPage from '@/pages/register/RegisterPage'
+import { Metadata } from 'next'
 import { FC } from 'react'
+
+import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+
+import RegisterPage from '@/pages/register/RegisterPage'
+
+export const metadata: Metadata = {
+	title: 'Зарегистрироваться',
+	...NO_INDEX_PAGE
+}
 
 const Page: FC = () => {
 	return <RegisterPage />
