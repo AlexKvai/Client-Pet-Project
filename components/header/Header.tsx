4957734acad2e21c70551b73e892e4c0bb883886
alex parfenov <alexp-2004@mail.ru>
2024-03-24 16:24:22 +0700

    Обновление header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,19 +1,43 @@
+'use client'
+
+import { useMutation } from '@tanstack/react-query'
+import Cookies from 'js-cookie'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { FC } from 'react'
 
+import { EnumTokens } from '@/services/auth-token.service'
+import { authService } from '@/services/auth.service'
+
 const Header: FC = () => {
+	const router = useRouter()
+	const { mutate } = useMutation({
+		mutationKey: ['logout'],
+		mutationFn: () => authService.logout(),
+		onSuccess: () => router.push('/auth')
+	})
+
 	return (
-		<div className='flex flex-row justify-between border-solid border-2 border-sky-500 py-[20px] px-[20px]'>
-			<div className='flex flex-row gap-x-8'>
-				<Link href={'/'}>Сайт ТОС(Актуальная информация)</Link>
-				<Link href={'/contacts'}>Связаться с председателем</Link>
+		<>
+			<div className='flex flex-row justify-between border-solid border-2 border-sky-500 py-[20px] px-[20px]'>
+				<div className='flex flex-row gap-x-8'>
+					<Link href={'/'}>Сайт ТОС(Актуальная информация)</Link>
+					<Link href={'/contacts'}>Связаться с председателем</Link>
+				</div>
+				<div className='flex flex-row gap-x-8'>
+					{!Cookies.get(EnumTokens.ACCESS_TOKEN) ? (
+						<>
+							<Link href={'/auth'}>Войти</Link>
+							<Link href={'/register'}>Зарегистрироваться</Link>
+						</>
+					) : (
+						<div>
+							<button onClick={() => mutate()}>Выйти</button>
+						</div>
+					)}
+				</div>
 			</div>
-			<div className='flex flex-row gap-x-8'>
-				<Link href={'/auth'}>Войти</Link>
-				<Link href={'/register'}>Зарегистрироваться</Link>
-			</div>
-		</div>
+		</>
 	)
 }
-
 export default Header
