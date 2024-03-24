708c33dc626ce57adc52c29941fa3c72d1681d84
alex parfenov <alexp-2004@mail.ru>
2024-03-24 16:26:49 +0700

    Добавление страницы авторизации

--- pages/auth/AuthPage.tsx
+++ pages/auth/AuthPage.tsx
@@ -1,7 +1,50 @@
+'use client'
+
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
 import { FC } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
+
+import { IAuthForm } from '@/types/auth.types'
+
+import { PAGES } from '@/config/pages-url.config'
+
+import { authService } from '@/services/auth.service'
 
 const AuthPage: FC = () => {
-	return <div>AuthPage</div>
+	const { register, handleSubmit, reset } = useForm<IAuthForm>({
+		mode: 'onChange'
+	})
+
+	const { push } = useRouter()
+
+	const { mutate } = useMutation({
+		mutationKey: ['login'],
+		mutationFn: (data: IAuthForm) => authService.login(data),
+		onSuccess() {
+			toast.success('Успешная авторизация')
+			reset()
+			push(PAGES.HOME)
+		}
+	})
+
+	const onSubmit: SubmitHandler<IAuthForm> = data => {
+		mutate(data)
+	}
+
+	return (
+		<div className='flex min-h-screen'>
+			<form
+				className='w-1/4 m-auto shadow rounded-xl'
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<input {...register('email', { required: true })} />
+				<input {...register('password', { required: true })} />
+				<button type='submit'>Войти</button>
+			</form>
+		</div>
+	)
 }
 
 export default AuthPage
