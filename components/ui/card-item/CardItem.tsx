605b379130717e5f9674be3798d6fa4e082b3c2c
alex parfenov <alexp-2004@mail.ru>
2024-03-24 16:24:39 +0700

    Рефакторинг cardItem

--- components/ui/card-item/CardItem.tsx
+++ components/ui/card-item/CardItem.tsx
@@ -5,15 +5,19 @@ import { ICard } from '@/types/card.types'
 
 const CardItem: FC<ICard> = ({ title, description }) => {
 	return (
-		<div className='flex flex-col h-[300px] w-[350px] items-center justify-center border-solid border-2 border-sky-500 mx-[20px] my-[20px]'>
-			<Image
-				src='https://4tololo.ru/sites/default/files/images/20181407165729.jpg'
-				width={250}
-				height={250}
-				alt='Picture of the author'
-			/>
-			<p>{title}</p>
-			<p>{description}</p>
+		<div className='flex flex-row h-[255px] w-full items-start justify-start border-solid border-2 border-sky-500 mt-[10px]'>
+			<div>
+				<Image
+					src='https://4tololo.ru/sites/default/files/images/20181407165729.jpg'
+					width={250}
+					height={250}
+					alt='Picture of the author'
+				/>
+			</div>
+			<div className='flex flex-col'>
+				<p>{title}</p>
+				<p>{description}</p>
+			</div>
 		</div>
 	)
 }
