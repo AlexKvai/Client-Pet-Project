a3664fdc87fe918c43fd77111de31a164fb7dfb1
alex parfenov <alexp-2004@mail.ru>
2024-03-26 20:16:56 +0700

    Создание страницы создания статьи

---
+++ app/article/create/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+import CreateArticlePage from '@/pages/create-article/CreateArticlePage'
+
+const Page: FC = () => {
+	return <CreateArticlePage />
+}
+
+export default Page
