2a49e97b3a2bcc887016b45da799551874f889b4
alex parfenov <alexp-2004@mail.ru>
2024-03-26 22:56:46 +0700

    Добавление seo для страницы с обновлением статьи

--- app/article/update/[id]/page.tsx
+++ app/article/update/[id]/page.tsx
@@ -1,6 +1,13 @@
+import { Metadata } from 'next'
 import { FC } from 'react'
 
 import UpdateArticlePage from '@/pages/update-article/UpdateArticlePage'
+
+export async function generateMetadata({
+	params: { id }
+}: any): Promise<Metadata> {
+	return { title: `Обновление статьи с id ${id}` }
+}
 
 const Page: FC = ({ params }: any) => {
 	return <UpdateArticlePage params={params} />
