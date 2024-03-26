737ab09affccdba3e7cbd596b4a8aef17cb078b5
alex parfenov <alexp-2004@mail.ru>
2024-03-26 22:57:13 +0700

    Добавление seo для страницы связаться с председателем

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,5 +1,14 @@
-import ContactPage from '@/pages/contacts/ContactPage'
+import { Metadata } from 'next'
 import { FC } from 'react'
+
+import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+
+import ContactPage from '@/pages/contacts/ContactPage'
+
+export const metadata: Metadata = {
+	title: 'Связаться с председателем',
+	...NO_INDEX_PAGE
+}
 
 const Page: FC = () => {
 	return <ContactPage />
