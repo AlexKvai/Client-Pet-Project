c951af1a63c411ddab09eb38c8b5cf721e69ba94
alex parfenov <alexp-2004@mail.ru>
2024-03-26 19:21:45 +0700

    Обновлена логика проверки Loader + изменение ссылки на создание статьи

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -30,41 +30,37 @@ const Header: FC = () => {
 		queryKey: ['getProfile'],
 		queryFn: () => userService.getProfile()
 	})
-	return (
-		<>
-			{isAuth && isLoading ? (
-				<Loader />
-			) : (
-				<div className='flex flex-row justify-between border-solid border-2 border-sky-500 py-[20px] px-[20px]'>
-					<div className='flex flex-row gap-x-8'>
-						<Link href={'/'}>Сайт ТОС(Актуальная информация)</Link>
-						<Link href={'/contacts'}>Связаться с председателем</Link>
-					</div>
-					<div className='flex flex-row gap-x-8'>
-						{!isAuth ? (
-							<>
-								<Link href={'/auth'}>Войти</Link>
-								<Link href={'/register'}>Зарегистрироваться</Link>
-							</>
-						) : (
-							<>
-								{data?.role === 'admin' && (
-									<Link href={'/article'}>Создать статью</Link>
-								)}
-
-								<Button
-									title='Выйти'
-									onClick={() => {
-										toast.success('Успешный выход')
-										mutate()
-									}}
-								/>
-							</>
+	return isAuth && isLoading ? (
+		<Loader />
+	) : (
+		<div className='flex flex-row justify-between border-solid border-2 border-sky-500 py-[20px] px-[20px]'>
+			<div className='flex flex-row gap-x-8'>
+				<Link href={'/'}>Сайт ТОС(Актуальная информация)</Link>
+				<Link href={'/contacts'}>Связаться с председателем</Link>
+			</div>
+			<div className='flex flex-row gap-x-8'>
+				{!isAuth ? (
+					<>
+						<Link href={'/auth'}>Войти</Link>
+						<Link href={'/register'}>Зарегистрироваться</Link>
+					</>
+				) : (
+					<>
+						{data?.role === 'admin' && (
+							<Link href={'/article/create'}>Создать статью</Link>
 						)}
-					</div>
-				</div>
-			)}
-		</>
+
+						<Button
+							title='Выйти'
+							onClick={() => {
+								toast.success('Успешный выход')
+								mutate()
+							}}
+						/>
+					</>
+				)}
+			</div>
+		</div>
 	)
 }
 export default dynamic(() => Promise.resolve(Header), { ssr: false })
