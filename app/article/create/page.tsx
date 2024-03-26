8db45ac5b3e035104e20ca4f16facd501ecaa6b4
alex parfenov <alexp-2004@mail.ru>
2024-03-26 22:55:56 +0700

    Обновление seo для создания статьи

--- app/article/create/page.tsx
+++ app/article/create/page.tsx
@@ -1,6 +1,14 @@
+import { Metadata } from 'next'
 import { FC } from 'react'
 
+import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+
 import CreateArticlePage from '@/pages/create-article/CreateArticlePage'
+
+export const metadata: Metadata = {
+	title: 'Создание статьи',
+	...NO_INDEX_PAGE
+}
 
 const Page: FC = () => {
 	return <CreateArticlePage />
